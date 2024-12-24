4453c525f71fc8a1c6b210db0173359369f9804f
Vaibhav Chaudhari <vaibhav.chaudhari@appliedaiconsulting.com>
2024-12-24 14:50:19 +0530

    Run npm build and Name changes

--- src/components/chat/elements/DefaultScreen.vue
+++ src/components/chat/elements/DefaultScreen.vue
@@ -13,12 +13,12 @@
             v-if="headerText"
             class="vdb-c-flex vdb-c-w-full vdb-c-items-center vdb-c-justify-between"
           >
-            <!-- <span
+            <span
               class="vdb-c-line-clamp-2 vdb-c-inline-block vdb-c-max-w-[60%] vdb-c-font-bold md:vdb-c-line-clamp-none md:vdb-c-max-w-[80%] md:vdb-c-font-extrabold vdb-c-text-orange"
               :title="headerText"
             >
               {{ headerText }}
-            </span> -->
+            </span>
             <Button
               v-if="headerConfig.uploadButton"
               @click="$emit('upload-button-click')"
@@ -116,6 +116,87 @@
             <component v-if="query.component" :is="query.component"></component>
             <span v-else class="vdb-c-break-words"> {{ query.content }} </span>
           </p>
+        </div>
+      </div>
+    </div>
+    <!-- Demo/Preview Videos -->
+    <div class="vdb-c-flex vdb-c-flex-col vdb-c-gap-12">
+      <!-- Divider -->
+      <div class="vdb-c-mt-12 vdb-c-h-1 vdb-c-w-full vdb-c-bg-[#EFEFEF]"></div>
+
+      <!-- Headers -->
+      <div
+        class="vdb-c-text-base vdb-c-font-normal vdb-c-text-vdb-darkishgrey md:vdb-c-text-xl"
+      >
+        <!-- Demo Videos Header -->
+        <div v-if="showDemoVideos">
+      
+        </div>
+
+        <!-- Preview Videos Header -->
+        <div v-else>
+          <div
+            v-if="collectionName"
+            class="vdb-c-flex vdb-c-items-center vdb-c-justify-between vdb-c-gap-4"
+          >
+            <span class="vdb-c-w-4/6 vdb-c-text-sm md:vdb-c-text-xl">
+              <b> Videos </b> in {{ collectionName }}
+            </span>
+            <div class="vdb-c-flex vdb-c-items-center vdb-c-gap-16">
+              <Button
+                variant="secondary"
+                @click="$emit('view-all-videos-click')"
+                class="!vdb-c-px-8 !vdb-c-py-10"
+              >
+                <div
+                  class="vdb-c-flex vdb-c-items-center vdb-c-justify-between vdb-c-gap-6"
+                >
+                  <div class="vdb-c-text-sm vdb-c-font-medium">View all</div>
+                  <ChevronRightCircled class="vdb-c-hidden md:vdb-c-block" />
+                </div>
+              </Button>
+            </div>
+          </div>
+          <div v-else>
+            <div
+              class="vdb-c-inline-block vdb-c-h-[3rem] vdb-c-w-full vdb-c-animate-pulse vdb-c-rounded vdb-c-bg-roy"
+            ></div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Videos -->
+      <div
+        v-if="previewVideos"
+        class="vdb-c-mb-24 vdb-c-grid vdb-c-grid-cols-12 vdb-c-gap-12 sm:vdb-c-mb-32 sm:vdb-c-gap-18"
+      >
+        <div
+          v-for="(item, index) in previewVideos"
+          :key="`post-${item.id}`"
+          class="vdb-c-col-span-12 sm:vdb-c-col-span-6 md:vdb-c-col-span-4 lg:vdb-c-col-span-3"
+        >
+          <video-card
+            border-class="sm:vdb-c-hidden"
+            :item="item"
+            :border-b="true"
+            :index="index"
+            :variant="showDemoVideos ? 'hide-title' : 'default'"
+            @video-click="$emit('video-click', item)"
+          />
+        </div>
+      </div>
+
+      <!-- Videos Loader -->
+      <div
+        v-else
+        class="vdb-c-mb-24 vdb-c-grid vdb-c-grid-cols-12 vdb-c-gap-24 sm:vdb-c-mb-32 sm:vdb-c-gap-32"
+      >
+        <div
+          v-for="(item, index) in [1, 2, 3, 4]"
+          :key="`post-loading-${index}`"
+          class="vdb-c-col-span-12 sm:vdb-c-col-span-6 md:vdb-c-col-span-4 lg:vdb-c-col-span-3"
+        >
+          <video-card-loader :index="index" />
         </div>
       </div>
     </div>
